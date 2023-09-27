0a34c002a4d080e6760fe9f136af20fc658fe4ff
marianavilarim <n01603357@humber.ca>
2023-09-27 14:46:55 -0400

    New paragraph in the about section

---
+++ MainScript.js
@@ -0,0 +1,15 @@
+var FirstName="Mariana";
+var LastName= "Vilarim";
+var FullName= FirstName + " " + LastName;
+var x = 2;
+var y = 3;
+var w = x + y;
+const button = document.getElementById("Buttoncickable");
+console.log (FullName);
+console.log ("Five ="+ " " + w)
+function something(a){ var a="hey";
+button.innerHTML=a;}
+w++;
+console.log(w);
+
+//This project ... introduction about my self
